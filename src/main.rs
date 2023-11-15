ef56d55e4502e4cac47e053e97f460ae5fa63938
davidrackerby <davidrackerby@gmail.com>
2023-11-15 11:59:40 -0500

    Add plotpy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use plotpy::{generate3d, Contour, Plot, StrError};
+
+const OUT_DIR: &str = "out";
+
+fn main() -> Result<(), StrError> {
+    // generate (x,y,z) matrices
+    let n = 21;
+    let (x, y, z) = generate3d(-2.0, 2.0, -2.0, 2.0, n, n, |x, y| x * x - y * y);
+
+    // configure contour
+    let mut contour = Contour::new();
+    contour
+        .set_colorbar_label("temperature")
+        .set_colormap_name("terrain")
+        .set_selected_level(0.0, true);
+
+    // draw contour
+    contour.draw(&x, &y, &z);
+
+    // add contour to plot
+    let mut plot = Plot::new();
+    plot.add(&contour);
+    plot.set_labels("x", "y");
+
+    // save figure
+    let out_path = format!("{OUT_DIR}/plotpy/readme_contour.svg");
+    plot.save(&out_path)?;
+    Ok(())
 }
