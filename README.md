01dee9deb4baa21104da062d0947345e1a449afe
davidrackerby <davidrackerby@gmail.com>
2023-11-25 22:31:03 -0500

    Finish project (surely there's nothing else I need to do)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CSE-431-Honors
-Comparing the efficiency of the HashSet and BTreeMap from the Rust standard library.
+Comparing the efficiency of the HashSet and BTreeMap from the Rust standard library, as well as observing the effects of applying another hashing algorithm.
--- src/main.rs
+++ src/main.rs
@@ -10,62 +10,73 @@ use std::io::Write;
 
 const OUT_DIR: &str = "out";
 const SEED: u64 = 431;
-const NUM_ELEMENTS: usize = 10_000_000;
+const NUM_ELEMENTS: usize = 30_000_000;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    run::<hash_map::RandomState>("sip13")?;
-    run::<ahash::RandomState>("ahash")
+    // Pseudorandom data
+    run::<hash_map::RandomState>("sip13", true)?;
+    run::<ahash::RandomState>("ahash", true)?;
+
+    // All elements are distinct
+    run::<hash_map::RandomState>("sip13", false)?;
+    run::<ahash::RandomState>("ahash", false)
 }
 
-fn run<Hasher: Default + BuildHasher>(out_path: &str) -> Result<(), Box<dyn Error>> {
+fn run<Hasher: Default + BuildHasher>(out_path: &str, random_data: bool) -> Result<(), Box<dyn Error>> {
     let mut rng = ChaCha8Rng::seed_from_u64(SEED);
 
-    // Generate the random data
-    let data: Vec<usize> = (0..NUM_ELEMENTS).map(|_| rng.gen_range(0..usize::MAX)).collect();
+    // Either use pseudorandom data or have data[i] == i for unique elements
+    let data: Vec<usize> = if random_data {
+        (0..NUM_ELEMENTS).map(|_| rng.gen_range(0..usize::MAX)).collect()
+    } else {
+        (0..NUM_ELEMENTS).collect()
+    };
 
     // Copies
-    let mut hashmap_time: Vec<u128> = vec![0; NUM_ELEMENTS];
-    let mut btreemap_time: Vec<u128> = vec![0; NUM_ELEMENTS];
-    let mut total_hashmap_time = 0.0;
-    let mut total_btreemap_time = 0.0;
+    let mut hashset_time: Vec<u128> = vec![0; NUM_ELEMENTS];
+    let mut btreeset_time: Vec<u128> = vec![0; NUM_ELEMENTS];
+    let mut total_hashset_time = 0.0;
+    let mut total_btreeset_time = 0.0;
 
-    let mut hash_map: HashSet<usize, Hasher> = HashSet::default();
-    let mut btree_map: BTreeSet<usize> = BTreeSet::new();
+    let mut hash_set: HashSet<usize, Hasher> = HashSet::default();
+    let mut btree_set: BTreeSet<usize> = BTreeSet::new();
 
     let mut num_elements_processed = NUM_ELEMENTS;
 
     for i in 0..NUM_ELEMENTS {
         let val = data[i];
 
-        // HashMap
-        let now_hashmap = Instant::now();
-        hash_map.insert(val);
-        let hash_elapsed = now_hashmap.elapsed();
-        hashmap_time[i] = hash_elapsed.as_nanos();
-        total_hashmap_time += hash_elapsed.as_secs_f64();
+        // hashset
+        let now_hashset = Instant::now();
+        hash_set.insert(val);
+        let hash_elapsed = now_hashset.elapsed();
+        hashset_time[i] = hash_elapsed.as_nanos();
+        total_hashset_time += hash_elapsed.as_secs_f64();
 
-        // BTreemap
-        let now_btreemap = Instant::now();
-        btree_map.insert(val);
-        let btree_elapsed = now_btreemap.elapsed();
-        btreemap_time[i] = btree_elapsed.as_nanos();
-        total_btreemap_time += btree_elapsed.as_secs_f64();
+        // btreeset
+        let now_btreeset = Instant::now();
+        btree_set.insert(val);
+        let btree_elapsed = now_btreeset.elapsed();
+        btreeset_time[i] = btree_elapsed.as_nanos();
+        total_btreeset_time += btree_elapsed.as_secs_f64();
 
         // Stop after total elapsed time exceeds 3 seconds
-        if total_btreemap_time >= 3.0 || total_hashmap_time >= 3.0 {
+        if total_btreeset_time >= 3.0 || total_hashset_time >= 3.0 {
             num_elements_processed = i;
             break;
         }
     }
 
-    let nonzero_hashmap_times_in_ns= &hashmap_time[..num_elements_processed];
-    let nonzero_btreemap_times_in_ns= &btreemap_time[..num_elements_processed];
+    let nonzero_hashset_times_in_ns= &hashset_time[..num_elements_processed];
+    let nonzero_btreeset_times_in_ns= &btreeset_time[..num_elements_processed];
 
-    let hash_times_str: Vec<String> = nonzero_hashmap_times_in_ns.iter().map(|n| format!("{n}")).collect();
-    let tree_times_str: Vec<String> = nonzero_btreemap_times_in_ns.iter().map(|n| format!("{n}")).collect();
+    let hash_times_str: Vec<String> = nonzero_hashset_times_in_ns.iter().map(|n| format!("{n}")).collect();
+    let tree_times_str: Vec<String> = nonzero_btreeset_times_in_ns.iter().map(|n| format!("{n}")).collect();
 
-    let mut f_hash = File::create(format!("{OUT_DIR}/{out_path}-hash.txt"))?;
-    let mut f_tree = File::create(format!("{OUT_DIR}/{out_path}-tree.txt"))?;
+    let f_type = if random_data { "random" } else { "fixed"};
+
+    let mut f_hash = File::create(format!("{OUT_DIR}/{out_path}-{f_type}-hash.txt"))?;
+    let mut f_tree = File::create(format!("{OUT_DIR}/{out_path}-{f_type}-tree.txt"))?;
 
     writeln!(f_hash, "{}", hash_times_str.join("\n"))?;
     writeln!(f_tree, "{}", tree_times_str.join("\n"))?;
